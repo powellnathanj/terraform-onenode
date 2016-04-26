383fbb3a25b69304714deb90c338a365503e2d12
Nathan Powell <nathan@nathanpowell.org>
2016-04-25 21:51:30 -0400

    Messing around

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
+resource "aws_vpc" "dev" {
+  cidr_block = "10.0.0.0/16"
+  tags {
+    Name = "dev.nathanpowell.org"
+    persistance = "destroy"
+  }
+}
+
+resource "aws_subnet" "devprivate" {
+  vpc_id     = "${aws_vpc.dev.id}"
+  cidr_block =  "10.0.122.0/24"
+  tags {
+    Name = "devprivate"
+    persistance = "destroy"
+  }
+}
+
+resource "aws_subnet" "devpublic" {
+  vpc_id     = "${aws_vpc.dev.id}"
+  cidr_block =  "10.0.124.0/24"
+  tags {
+    Name = "devpublic"
+    persistance = "destroy"
+  }
+}
