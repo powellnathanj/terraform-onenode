754edb01eee9f481cb8a007940d588235d8e500a
Nathan Powell <nathan@nathanpowell.org>
2016-04-25 22:27:20 -0400

    Adding a bit more

--- main.tf
+++ main.tf
@@ -29,3 +29,37 @@ resource "aws_subnet" "devpublic" {
     persistance = "destroy"
   }
 }
+
+resource "aws_security_group" "devsg" {
+  vpc_id = "${aws_vpc.dev.id}"
+  name   = "basic"
+  tags {
+    persistance = "destroy"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+}
+
+resource "aws_internet_gateway" "devgw" {
+  vpc_id = "${aws_vpc.dev.id}"
+}
